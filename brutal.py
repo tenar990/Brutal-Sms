64919d8bb766bba0a9e4b8e6263372ed6e7925eb
Raden Sanz Soekamti <51875646+B4N954N2-ID@users.noreply.github.com>
2020-06-16 07:24:02 +0700

    Create brutal.py

---
+++ brutal.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('clear')
+print 'Comingsoon in the Next Day Cuk ^_^'
+print 'Lagi diperbaiki dulu ada yg error!!'
+print 'Sabar Woke ^_^\nTunggu Update-an nya di Channel Youtube Sanz Soekamti'
