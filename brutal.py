fafc116e4cfdf246cad0f6cd5b5d1f10a51ed450
Raden Sanz Soekamti <51875646+B4N954N2-ID@users.noreply.github.com>
2020-06-17 06:43:31 +0700

    Delete brutal.py

--- brutal.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system('clear')
-print 'Comingsoon in the Next Day Cuk ^_^'
-print 'Lagi diperbaiki dulu ada yg error!!'
-print 'Sabar Woke ^_^\nTunggu Update-an nya di Channel Youtube Sanz Soekamti'
